23ec04e0cbe828f1cf5f6058545dc012cf3ebb8e
Muuted <adam.s.knudsen@gmail.com>
2024-09-12 19:01:16 +0200

    sdf

--- Diffusion_sim.py
+++ Diffusion_sim.py
@@ -1,19 +1,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy as sp
-from diff_sim_funcs import dCdt
+from diff_sim_funcs import dCondt, check_volume
 
 
 # Concentrations for Ca^2+ inside; outside cell
-c_out = 5#2e-3 #M Concentration outside of cell
-c_in = -1#3e-9 #M Concetration inside cell
+c_out = 2e-3 #M Concentration outside of cell
+c_in = 100e-9 #M Concetration inside cell
 
+# time and step size, and diffusion constant
+dx, dy = 1,1
+dt = 0.01
+D  = 0.1
+holesize = 2
 
 # Creation of our Grid
-T_tot = 10
-Ysize = 5
+T_tot = 1000
+Ysize = 20
 Xsize = 2*Ysize + 1
-Grid_diff = np.zeros(shape=(T_tot,Ysize,Xsize)) # replication of figure 1.b
+Grid_diff = np.zeros(shape=(2,Ysize,Xsize)) # replication of figure 1.b
 
 # Initilizing our Grid. leaving a barrier in the middle,
 # that seperates the outside(left) and inside(right) of the cell
@@ -25,28 +30,30 @@ for x in range(1,Xsize-1):
             Grid_diff[0][y][x] = c_in
 
 
-#print(Grid_diff[0])
-N = 100
-test_grid = np.zeros(shape=(2,N,N))
-t = 0
-T= 100
-test_grid[0][int(N/2)][int(N/2)] = 100 
+tot_conc1 = check_volume(grid=Grid_diff[0])
 
-D,dx,dy,dt = 1,1,1,1
+for t in range(0,T_tot-1):
 
+    t1, t2 = t%2, (t+1)%2 # to change the matricies back and forth
 
-plt.matshow(test_grid[0])
-plt.show()
+    for x in range(1,Xsize - 1):
+        for y in range(1,Ysize - 1):
 
-while t < T:
-    if t%10:
-        print(f"time ellapsed={t}, of total time={T}")
-    
-    for x in range(1,N-1):
-        for y in range(1,N-1):
-            change_val = dt*D*dCdt(C=test_grid[(t+1)%2],D=D,x=x,y=y,dx=dx,dy=dy,dt=dt)
-            test_grid[t%2][y][x] = test_grid[(t+1)%2][y][x] + change_val
+            if x != Ysize:
+                dCdt = dCondt(C=Grid_diff[t1]
+                              ,x=x,y=y,dx=dx,dy=dy, h=1
+                              )
+                Grid_diff[t2][y][x]= Grid_diff[t1][y][x] + D*dt*dCdt
+            
+            if x == Ysize:
+                if(Ysize - holesize)/2 < y < (Ysize + holesize)/2:
+                    dCdt = dCondt(C=Grid_diff[t1]
+                                  ,x=x,y=y,dx=dx,dy=dy,h=1
+                                  )
+                    Grid_diff[t2][y][x] = Grid_diff[t1][y][x] + D*dt*dCdt
 
-    t += 1
-plt.matshow(test_grid[0])
+tot_conc2 = check_volume(grid=Grid_diff[0])
+
+print(f"conc2/conc1 ={tot_conc2/tot_conc1}")
+plt.matshow(Grid_diff[0])
 plt.show()
--- diff_sim_funcs.py
+++ diff_sim_funcs.py
@@ -2,25 +2,86 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy as sp
 
+def check_volume(grid)-> float:
+    Ysize,Xsize = np.shape(grid)
+    
+    tot_conc = 0
+    for x in range(Xsize):
+        for y in range(Ysize):
+            tot_conc += grid[y][x]
+    
+    return tot_conc
 
 
 
-def dCdt(C,D,x,y,dx,dy,dt):
+def dCondt(C,x,y,dx,dy,h) -> float:
 
-    dcdxdx = (C[y][x + dx] - 2*C[y][x] + C[y][x-dx])/(dx^2)
+    dcdxdx = (C[y][x + dx] - 2*C[y][x] + C[y][x-dx])/(dx**2)
 
-    dcdydy = (C[y + dy][x] - 2*C[y][x] + C[y-dy][x])/(dy^2)
+    dcdydy = (C[y + dx][x] - 2*C[y][x] + C[y-dx][x])/(dy**2)
    
     dcdt = dcdxdx + dcdydy 
     
     return dcdt
 
+
+def RungeKutta(C,x,y,dx,dy,h):
+
+    #first part Runge Kutta
+    dx,dy = 2*dx, 2*dy
+    dcdxdx = (C[y][x+dx] - 2*C[y][x] + C[y][x-dx])/(dx**2)
+
+    dcdydy = (C[y+dy][x] - 2*C[y][x] + C[y-dy][x])/(dy**2)
+   
+    k1x,ky1 = dcdxdx, dcdydy 
+
+    # 2nd part Runge kutta
+    dx, dy = dx/2 , dy/2
+    dcdxdx = (C[y][x+dx] - 2*C[y][x] + C[y][x-dy] )/(dx**2)
+
+    dcdydy = (C[y+dy][x] - 2*C[y][x] + C[y-dx][x])/(dy**2)
+
+    k2 = dcdxdx + dcdydy 
+
+
     
+def test():
+    #Testing diff equation setup.
+    #print(Grid_diff[0])
+    N = 10
+    test_grid = np.zeros(shape=(2,N,N))
+    t = 0
+    T= 100
+    test_grid[0][int(N/2)][int(N/2)] = 1000
+
+    D,dt = 0.1,0.1 # there is something about numerical stability
+    dx = 1         # where dt/(dx)^2 < 0.5 or something like that.
+    dy = dx
+
+    plt.matshow(test_grid[0])
+
+
+    for t in range(T):
+        if t%10==0:
+            print(f"time ellapsed={t}, of total time={T}")
+
+
+        t1, t2 = t%2, (t+1)%2
+        
+        for x in range(1,N-1):
+            for y in range(1,N-1):
+                change_val = dt*D*dCdt(C=test_grid[t1]
+                                    ,x=x,y=y,dx=dx,dy=dy
+                                    )
+                test_grid[t2][y][x] = test_grid[t1][y][x] + change_val
+
+
+    plt.matshow(test_grid[0])
+    plt.show()
 
 
 
 
-"""
+
 if __name__ == "__main__":
     pass
-"""
